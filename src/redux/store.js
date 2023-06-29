47d57524b3c42f66c335aa8dfffdd4904623eee9
“Alex <korolenkosasha644@gmail.com>
2023-06-29 16:22:30 +0300

    install library

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+﻿import { combineReducers, applyMiddleware, legacy_createStore as createStore} from 'redux';
+
+let reduser = combineReducers({
+    
+});
+
+const store = createStore(reducers, applyMiddleware());
+
+export default store;
