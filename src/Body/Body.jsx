fc99554e0033b9b2de433509f021e4ddcaac3fb0
“Alex <korolenkosasha644@gmail.com>
2023-07-01 12:59:26 +0300

    Add NavBar

---
+++ src/Body/Body.jsx
@@ -0,0 +1,14 @@
+﻿import React from "react";
+import NavBar from "./Nav/NavBar";
+
+const Body = (props) => {
+    return (
+        <div className="row mt-12">
+            <div className="">
+                <NavBar/>
+            </div>
+        </div>
+    )
+}
+
+export default Body;
---
+++ src/Body/BodyContainer.jsx
@@ -0,0 +1,11 @@
+﻿import React from "react";
+import Body from "./Body";
+
+class BodyContainer extends React.Component{
+    render() {
+        return <Body/>
+    }
+    
+}
+
+export default BodyContainer;
